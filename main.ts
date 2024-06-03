4d95e715c8f18e3d0858d6aa78a966d08a03e472
samia-urooj <samiaurooj386@gmail.com>
2024-06-03 07:46:00 -0700

    created adventure game

---
+++ main.ts
@@ -0,0 +1,217 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+class Player {
+  name: string;
+  fuel: number = 100;
+  constructor(name: string) {
+    this.name = name;
+  }
+  fuelDecrease(){
+    let fuel = this.fuel - 25
+    this.fuel = fuel
+  }
+  fuelIncrease(){
+    this.fuel = 100;
+  }
+}
+
+class Opponent {
+  name: string;
+  fuel: number = 100;
+  constructor(name: string) {
+    this.name = name;
+  }
+  fuelDecrease(){
+    let fuel = this.fuel - 25
+    this.fuel = fuel
+  }
+}
+
+let player = await inquirer.prompt([
+  {
+    name: "name",
+    type: "input",
+    message: "Enter Your Name",
+  },
+]);
+let opponent = await inquirer.prompt([
+  {
+    name: "select",
+    type: "list",
+    choices: ["Spiderman", "Batman", "Doctor strange", "Hulk"],
+  },
+]);
+let p1 = new Player(player.name);
+let o1 = new Opponent(opponent.select);
+
+do{
+    if(opponent.select == "Spiderman"){
+        let ask = await inquirer.prompt([
+            {
+                name : "opt",
+                type : "list",
+                message : "What would you like to do?",
+                choices: ["Attack", "Drink Portion", "Run for your life.."]
+            }
+        ]);
+        if (ask.opt == "Attack"){
+            let num = Math.floor(Math.random() * 2)
+            if (num > 0){
+                p1.fuelDecrease()
+                console.log(`${p1.name} fuel is ${p1.fuel}`);
+                console.log(`${o1.name} fuel is ${o1.fuel}`);
+                if(p1.fuel <= 0){
+                  console.log(`You loose, Game Over!`);
+                  process.exit()
+                }
+            }
+            if (num <= 0){
+                o1.fuelDecrease()
+                console.log(`${p1.name} fuel is ${p1.fuel}`);
+                console.log(`${o1.name} fuel is ${o1.fuel}`);
+                
+                if(o1.fuel <= 0){
+                  console.log(`Congrats! You win.`);
+                  process.exit()
+               }
+            }
+        }
+        if(ask.opt == "Drink Portion"){
+          p1.fuelIncrease()
+          console.log(`You drink health portion! Now your fuel is ${p1.fuel}`)
+        }
+        if(ask.opt == "Run for your life.."){
+            console.log(`You loose, Game Over!`)
+            process.exit()
+        }
+    }
+
+    //batman
+    if(opponent.select == "Batman"){
+      let ask = await inquirer.prompt([
+          {
+              name : "opt",
+              type : "list",
+              message : "What would you like to do?",
+              choices: ["Attack", "Drink Portion", "Run for your life.."]
+          }
+      ]);
+      if (ask.opt == "Attack"){
+          let num = Math.floor(Math.random() * 2)
+          if (num > 0){
+              p1.fuelDecrease()
+              console.log(`${p1.name} fuel is ${p1.fuel}`);
+              console.log(`${o1.name} fuel is ${o1.fuel}`);
+              if(p1.fuel <= 0){
+                console.log(`You loose, Game Over!`);
+                process.exit()
+              }
+          }
+          if (num <= 0){
+              o1.fuelDecrease()
+              console.log(`${p1.name} fuel is ${p1.fuel}`);
+              console.log(`${o1.name} fuel is ${o1.fuel}`);
+              
+              if(o1.fuel <= 0){
+                console.log(`Congrats! You win.`);
+                process.exit()
+             }
+          }
+      }
+      if(ask.opt == "Drink Portion"){
+        p1.fuelIncrease()
+        console.log(`You drink health portion! Now your fuel is ${p1.fuel}`)
+      }
+      if(ask.opt == "Run for your life.."){
+          console.log(`You loose, Game Over!`)
+          process.exit()
+      }
+  }
+
+  //doctor strange
+  if(opponent.select == "Doctor strange"){
+    let ask = await inquirer.prompt([
+        {
+            name : "opt",
+            type : "list",
+            message : "What would you like to do?",
+            choices: ["Attack", "Drink Portion", "Run for your life.."]
+        }
+    ]);
+    if (ask.opt == "Attack"){
+        let num = Math.floor(Math.random() * 2)
+        if (num > 0){
+            p1.fuelDecrease()
+            console.log(`${p1.name} fuel is ${p1.fuel}`);
+            console.log(`${o1.name} fuel is ${o1.fuel}`);
+            if(p1.fuel <= 0){
+              console.log(`You loose, Game Over!`);
+              process.exit()
+            }
+        }
+        if (num <= 0){
+            o1.fuelDecrease()
+            console.log(`${p1.name} fuel is ${p1.fuel}`);
+            console.log(`${o1.name} fuel is ${o1.fuel}`);
+            
+            if(o1.fuel <= 0){
+              console.log(`Congrats! You win.`);
+              process.exit()
+           }
+        }
+    }
+    if(ask.opt == "Drink Portion"){
+      p1.fuelIncrease()
+      console.log(`You drink health portion! Now your fuel is ${p1.fuel}`)
+    }
+    if(ask.opt == "Run for your life.."){
+        console.log(`You loose, Game Over!`)
+        process.exit()
+    }
+}
+
+//hulk
+if(opponent.select == "Hulk"){
+  let ask = await inquirer.prompt([
+      {
+          name : "opt",
+          type : "list",
+          message : "What would you like to do?",
+          choices: ["Attack", "Drink Portion", "Run for your life.."]
+      }
+  ]);
+  if (ask.opt == "Attack"){
+      let num = Math.floor(Math.random() * 2)
+      if (num > 0){
+          p1.fuelDecrease()
+          console.log(`${p1.name} fuel is ${p1.fuel}`);
+          console.log(`${o1.name} fuel is ${o1.fuel}`);
+          if(p1.fuel <= 0){
+            console.log(`You loose, Game Over!`);
+            process.exit()
+          }
+      }
+      if (num <= 0){
+          o1.fuelDecrease()
+          console.log(`${p1.name} fuel is ${p1.fuel}`);
+          console.log(`${o1.name} fuel is ${o1.fuel}`);
+          
+          if(o1.fuel <= 0){
+            console.log(`Congrats! You win.`);
+            process.exit()
+         }
+      }
+  }
+  if(ask.opt == "Drink Portion"){
+    p1.fuelIncrease()
+    console.log(`You drink health portion! Now your fuel is ${p1.fuel}`)
+  }
+  if(ask.opt == "Run for your life.."){
+      console.log(`You loose, Game Over!`)
+      process.exit()
+  }
+}
+}
+while(true)
